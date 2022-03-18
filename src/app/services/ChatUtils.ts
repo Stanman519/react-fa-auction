9abc4d246f63d9381d29a7b5bf19881e4dad1eb4
RyanStanley <your_email_address@example.com>
2022-03-18 08:09:35 -0400

    stream key fix

--- src/app/services/ChatUtils.ts
+++ src/app/services/ChatUtils.ts
@@ -1,4 +1,4 @@
 import { StreamChat } from "stream-chat";
 
-export const apiKey = process.env.STREAM_KEY;
+export const apiKey = process.env.REACT_APP_STREAM_KEY;
 //export const chatClient = StreamChat.getInstance(apiKey);
