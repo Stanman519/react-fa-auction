a3cd004d5e5431730ff8707b439a8ecfe74cd071
Stanman519 <Stanman519@gmail.com>
2024-07-22 11:11:14 -0400

    import { RootState } from "../store";

--- src/app/components/AuctionTeamSalaryCapsSlab.tsx
+++ src/app/components/AuctionTeamSalaryCapsSlab.tsx
@@ -1,59 +1,53 @@
 import { Avatar, Box, Divider, Drawer, List, ListItem, ListItemText, useTheme } from "@mui/material"
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../redux/reducers/RootReducer";
+
 import { updateUI } from "../redux/actions/UiActions";
 import { useMemo } from "react";
 import { OpposingFranchiseDTO } from "../redux/reducers/OwnerReducer";
+import { RootState } from "../store";
 
 interface CurrentBids {
     ownerId: number;
     bidSalary: number;
 }
+
 const groupOwners = (owners: OpposingFranchiseDTO[]) => {
     const groupedOwners: { [key: number]: OpposingFranchiseDTO } = {};
-    console.log('re-running grouped owners', groupedOwners)
     owners.forEach(owner => {
         const { mflfranchiseid, ownerName, avatar } = owner;
 
         if (!groupedOwners[mflfranchiseid]) {
             groupedOwners[mflfranchiseid] = { ...owner, ownerName: '', avatar: avatar };
-        }
-
+        }3
         if (groupedOwners[mflfranchiseid].ownerName) {
             groupedOwners[mflfranchiseid].ownerName += ` AKA ${ownerName}`;
         } else {
             groupedOwners[mflfranchiseid].ownerName = ownerName;
         }
     });
-    console.log('returning grouped owners', groupedOwners)
     return Object.values(groupedOwners);
 };
+
 export const AuctionTeamSalaryCapsSlab = (): JSX.Element => {
     const owners = useSelector((state: RootState) => state.owners);
     const groupedOwners = useMemo(() => groupOwners(owners), [owners]);
     const { palette } = useTheme();
-    const {  currentLeague } = useSelector((state: RootState) => state.profile);
-    const openSlab = useSelector((state: RootState) => state.ui.modal === 'team-caps-slab')
+    const { currentLeague } = useSelector((state: RootState) => state.profile);
+    const openSlab = useSelector((state: RootState) => state.ui.modal === 'team-caps-slab');
     const dispatch = useDispatch();
     const lots = useSelector((state: RootState) => state.lots.filter(l => l.leagueId === currentLeague?.league.leagueId ?? 0));
 
-
-
-    // Memoize the high bids calculations for each owner
     const highBidsForOwners = useMemo(() => {
         const bids: { [key: number]: number } = {};
-        console.log('lots changed, lets run it.', bids)
         groupedOwners.forEach(owner => {
             bids[owner.leagueownerid] = lots
                 .filter(l => l.bid?.ownerId === owner.leagueownerid)
                 .map(b => b.bid?.bidSalary ?? 0)
                 .reduce((prev, curr) => prev + curr, 0);
         });
-        console.log('returning bids' ,bids)
         return bids;
     }, [lots, groupedOwners]);
 
- console.log('grouped onw', groupedOwners)
 
     return (
     <Drawer
