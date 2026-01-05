6a176f992e2bf0097c547b144dc95d4f8027adba
Stanley, Ryan <Ryan.Stanley@dcsg.com>
2026-01-05 09:30:01 -0500

    log in nav

--- src/app/components/nonAuction/LandingPage.tsx
+++ src/app/components/nonAuction/LandingPage.tsx
@@ -1,11 +1,23 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
+
 export const LandingPage = () => {
   const logo = "./stanfan-color-logo.png";
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+
+  useEffect(() => {
+    // If not loading and not authenticated, redirect to login
+    if (!isLoading && !isAuthenticated) {
+      loginWithRedirect({
+        appState: { returnTo: window.location.pathname }
+      });
+    }
+  }, [isLoading, isAuthenticated, loginWithRedirect]);
 
   return (
     <div className="flex flex-row justify-center items-center max-w-screen-sm min-h-screen ">
-      <div className="flex-col justify-center items-center max-w-full p-4 m-4 ">
+      <div className="flex flex-col justify-center items-center max-w-full p-4 m-4">
         <img className="max-w-xs animate-pulse" src={logo} />
-        {/* < Button onClick={() => loginWithRedirect()}> Log In</Button > */}
       </div>
     </div>
   );
